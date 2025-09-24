56c6c8104b855e56cd46b4d1de8bc28b70f611df
Jan Äärelä <jan.suomi.fin@gmail.com>
2025-09-24 08:58:54 +0300

    Updated configs

--- broot/dragon/dragon.sh
+++ broot/dragon/dragon.sh
@@ -4,4 +4,4 @@ sleep 0.25
 
 dragon-drop -x -s 256 -A $(cat ~/.config/broot/dragon/dragon_list.txt)
 
-echo "" > ~/.config/broot/dragon/dragon_list.txt
+echo "" >~/.config/broot/dragon/dragon_list.txt
