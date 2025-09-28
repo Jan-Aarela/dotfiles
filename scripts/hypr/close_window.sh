15a6eefcdf0864d9531fa35969a879aa7acad740
Jan Äärelä <jan.suomi.fin@gmail.com>
2025-09-28 15:13:03 +0300

    Moved scipts into scripts directory.

--- scripts/hypr/close_window.sh
+++ scripts/hypr/close_window.sh
@@ -10,16 +10,14 @@
 #   hyprctl dispatch killactive
 # fi
 
-
-
-ADDRESS1=$(cat ~/.config/hypr/scripts/close_variable)
+ADDRESS1=$(cat ~/.config/scripts/hypr/close_variable)
 ADDRESS2=$(hyprctl activewindow -j | jq '.address')
 
 if [[ "$ADDRESS2" == "$ADDRESS1" ]]; then
-  echo 0 > ~/.config/hypr/scripts/close_variable
+  echo 0 >~/.config/scripts/hypr/close_variable
   hyprctl dispatch killactive
 else
-  echo "$ADDRESS2" > ~/.config/hypr/scripts/close_variable
+  echo "$ADDRESS2" >~/.config/scripts/hypr/close_variable
   sleep 0.25
-  echo 0 > ~/.config/hypr/scripts/close_variable
+  echo 0 >~/.config/scripts/hypr/close_variable
 fi
